624b22fc6071d55cefa6f2d17301d802c5614bfa
Emigdio <emigdioriz@gmail.com>
2024-10-04 09:38:13 -0300

    Criando projeto

---
+++ src/components/IngredienteSelecionavel.vue
@@ -0,0 +1,37 @@
+<script land="ts">
+    import Tag from './Tag.vue';
+
+    export default {
+        props: {
+            ingrediente: {type: String, required: true}
+        },
+        components: {Tag},
+        data() {
+            return {
+                selecionado : false
+            }
+        },
+        methods: {
+            aoClicar() {
+                this.selecionado = !this.selecionado;
+
+                if (this.selecionado) {
+                    this.$emit('adicionarIngrediente', this.ingrediente);
+                }
+            }
+        },
+        emits: ['adicionarIngrediente'] // declarando eventos personalisado que o componente emite
+    }  
+</script>
+
+<template>
+    <button class="ingrediente" v-on:click="aoClicar()" :aria-pressed="selecionado"> <!-- v-on pode ser substituido por @click -->
+        <Tag :texto="ingrediente" :ativa="selecionado" />
+    </button>
+</template>
+
+<style scoped>
+    .ingrediente {
+        cursor: pointer;
+    }
+</style>
